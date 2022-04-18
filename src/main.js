50eb9716c61ad6accaa9fc0d4007a4f2f6c9c5c4
silmist <70921116+silmist@users.noreply.github.com>
2022-04-18 00:08:24 -0700

    commit on 'Main'

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-console.log("Hello from main dot js");
+//Points Breakdown:
+//60 for Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi)
+    //Ice cream float aesthetic
+//10 for Display the time remaining (in seconds) on the screen
+//20 for Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points
+    //The small ice cream on a jetski
+//10 for Implement parallax scrolling
+
+//100 points total
+
+
+//Most base code taken from the Rocket Patrol Origins Tutorial provided in previous assignment
 
 
 
@@ -17,5 +28,3 @@ let borderUISize = config.height / 15;
 let borderPadding = borderUISize / 3;
 
 let game = new Phaser.Game(config);
-
-//let game= new phaser.Game();
